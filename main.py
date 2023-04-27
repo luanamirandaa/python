114d72f654b86c7a0922b3270135ecf393e12fab
luanamirandaa <luana.miranda@thoughtworks.com>
2023-04-26 23:14:54 -0300

    criando primeira condicional if else

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print ("hello world")
 print ("Meu Nome e Luana")
 print ("Eu tenho " + "31 anos")
+
+valor = input("digite 1 ou 2")
+if valor == "1":
+    print ("obrigada richard")
+elif valor == "2":
+    print ("vamos ficar ricos")
